4ad70a6cab8249fbc4c4eccfaaae272f3ef087be
Christian Ilagan <cmilagan7@gmail.com>
2021-08-11 20:30:33 +1000

    restructuring code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { AiFillLinkedin, AiFillMail, AiFillPhone, AiOutlineDribbble } from "react-icons/ai";
 import "./App.css"
-import Navbar from './Components/Navbar';
-
+import { AiOutlineSortAscending, AiOutlineSortDescending } from "react-icons/ai";
+import "./Navbar.css"
 class App extends Component {
   constructor(props) {
     super(props);
@@ -22,6 +22,8 @@ class App extends Component {
         })
       });
   }
+
+  
   
   render() {
 
@@ -33,10 +35,27 @@ class App extends Component {
       );
     }
     // sorting alphabetically
-    items.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+    function asc() {
+      items.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+    }
+
+    function desc() {
+      items.sort((a,b) => (a.name > b.name) ? -1 : ((b.name > a.name) ? 1 : 0));
+    }
     return (
       <div className="App">
-        <Navbar/>
+        <div className="navbar">
+            <div className="left">
+                
+            </div>
+            <input type="text" placeholder="Search.."/>
+            <button onClick={asc}>
+                <AiOutlineSortAscending size="24px" color="black"/>
+            </button>
+            <button onClick={desc}>
+                <AiOutlineSortDescending size="24px" color="black"/>
+            </button>
+        </div>
         <div className="container">
           <ul>
             {items.map(item => (
