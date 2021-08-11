e720b05a2832a0683183b8c6bb119f00a168c177
Christian Ilagan <cmilagan7@gmail.com>
2021-08-11 21:47:26 +1000

    Added sorting feature

--- src/App.js
+++ src/App.js
@@ -10,36 +10,43 @@ function App() {
 
   const [items, setItems] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
-    useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/users')
-            .then(res => {
-                console.log(res)
-                setItems(res.data)
-            })
-            .catch(err => {
-                console.log(err)   
-            })
+  const [asc, setAsc] = useState(false)
+  const [desc, setDesc] = useState(true)
 
-    }, [])
-  
+  useEffect(() => {
+      axios.get('https://jsonplaceholder.typicode.com/users')
+          .then(res => {
+              console.log(res)
+              setItems(res.data)
+          })
+          .catch(err => {
+              console.log(err)   
+          })
+
+  }, [])
+    if (asc) {
+      items.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+    }
+
+    if (desc) {
+      items.sort((a,b) => (a.name > b.name) ? -1 : ((b.name > a.name) ? 1 : 0));
+    }
     return (
       <div className="App">
         <div className="navbar">
-            <div className="left">
-                
-            </div>
             <input type="text" placeholder="Search.." onChange={event => {setSearchTerm(event.target.value)}}/>
-            <button>
-                <AiOutlineSortAscending size="24px" color="black"/>
+            <button onClick={() => {setAsc(true); setDesc(false)}}>
+              <AiOutlineSortAscending size="24px" color="black"/>
             </button>
-            <button>
-                <AiOutlineSortDescending size="24px" color="black"/>
+            <button onClick={() => {setAsc(false); setDesc(true)}}>
+            <AiOutlineSortDescending size="24px" color="black"/>
             </button>
+            
         </div>
         <div className="container">
           <ul>
             {items.filter((item) => {
-                if (searchTerm == "") {
+                if (searchTerm === "") {
                   return item
                 } else if (item.name.toLowerCase().includes(searchTerm.toLowerCase())) {
                   return item
